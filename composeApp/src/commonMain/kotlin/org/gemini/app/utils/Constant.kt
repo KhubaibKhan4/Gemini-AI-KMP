d7d21314f38450532a57a2779a17968b767d1f65
Khubaib Khan <18.bscs.803@gmail.com>
2023-12-14 16:37:53 +0500

    -Some Enhancement (#2)

--- composeApp/src/commonMain/kotlin/org/gemini/app/utils/Constant.kt
+++ composeApp/src/commonMain/kotlin/org/gemini/app/utils/Constant.kt
@@ -2,6 +2,6 @@ package org.gemini.app.utils
 
 object Constant {
     const val BASE_URL = "https://generativelanguage.googleapis.com/"
-    const val API_KEY = "AIzaSyAGIbCm970chMEFc5fEiOLp0pxFvlrcN8E"
+    const val API_KEY = ""
     const val TIMEOUT  = 300000L
 }
